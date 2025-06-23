b41d4ffb29ac0740c819b8c012c4ed7f3f2f2fc8
nandan kumar <nandanbharadwaj4@gmail.com>
2025-06-23 19:43:03 +0530

    added a new city model and migration using following command: // npx sequelize-cli model:generate --name City --attributes name:string,country:string // npx sequelize-cli db:migrate

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -17,6 +17,7 @@ module.exports = (sequelize, DataTypes) => {
     modelNumber:{
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
       validate: {
         isAlphanumeric: true, 
       }
