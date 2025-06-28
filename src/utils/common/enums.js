af59cb13647d56b064652679ec587fdab4eaea34
nandan kumar <nandanbharadwaj4@gmail.com>
2025-06-28 23:21:55 +0530

    fix: Update enums.js to include new status codes

--- src/utils/common/enums.js
+++ src/utils/common/enums.js
@@ -1,8 +1,12 @@
-const SEAT_TYPES = {
+const SEAT_TYPES = Object.freeze({
   ECONOMY: 'economy',
+  PREMIUM_ECONOMY: 'premium_economy',
+  ECONOMY_PLUS: 'economy_plus',       
   BUSINESS: 'business',
-  FIRST_CLASS: 'first-class',
-    PREMIUM_ECONOMY: 'premium_economy'
-};
+  FIRST_CLASS: 'first_class',
+  BULKHEAD: 'bulkhead',           
+  EXIT_ROW: 'exit_row',           
+  LUXURY_BUSINESS: 'luxury_business',  
+});
 
 module.exports = { SEAT_TYPES };
